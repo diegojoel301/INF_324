7270fa388d827165f61e3459cd5b0df9d8d84a7a
diegojoel301 <diegojoelcondoriquispe@gmail.com>
2023-04-05 21:22:22 +0200

    Main

--- python_clase_2/main.py
+++ python_clase_2/main.py
@@ -1,2 +1,7 @@
-print("hello World")
-print("mago")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/hola_mundo', methods = ['GET'])
+def hola_mundo():
+    return "<p>Hola Mundo</p>"
